7a45f6b4b0bcb3125ad084b0fefdb47f7df52106
Sanchiks07 <ipb23.s.mitniece@vtdt.edu.lv>
2025-11-26 12:34:28 +0200

    ieinstalēju breez api auth

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CasesController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
 
+require __DIR__.'/auth.php';
